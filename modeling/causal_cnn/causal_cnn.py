5a8646b6e3181c7bfe4add485c98217e39bd70b4
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2023-12-02 20:42:54 -0800

    update models

--- modeling/causal_cnn/causal_cnn.py
+++ modeling/causal_cnn/causal_cnn.py
@@ -16,11 +16,13 @@ import os
 import sys
 
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from data_handling.data_loading import league_data_loader
+
 
 # Data Preparation Class
 class SequenceDataset(Dataset):
@@ -32,58 +34,86 @@ class SequenceDataset(Dataset):
 
     def __getitem__(self, idx):
         sequence, ext_data, fga_weight, label = self.sequences[idx]
-        return torch.FloatTensor(sequence), torch.FloatTensor(ext_data), torch.FloatTensor([fga_weight]), torch.FloatTensor([label])
+        return (
+            torch.FloatTensor(sequence),
+            torch.FloatTensor(ext_data),
+            torch.FloatTensor([fga_weight]),
+            torch.FloatTensor([label]),
+        )
+
 
 def create_inout_sequences(input_data, features, response, start_index, lag=4):
     inout_seq = []
     input_data = input_data.reset_index(drop=True)
     for i in range(start_index, len(input_data)):
         end = i + lag
-        if end > len(input_data)-1:
+        if end > len(input_data) - 1:
             break
-        seq_data = input_data.loc[i:(end - 1), response].values.astype(float)
+        seq_data = input_data.loc[i : (end - 1), response].values.astype(float)
         ext_data = input_data.loc[end, features].values.astype(float)
         fga_weight = input_data.loc[end, "weights"]
         label = input_data.loc[end, response]
         inout_seq.append((seq_data, ext_data, fga_weight, label))
     return inout_seq
 
+
 def create_weights(input_data, weight_feature, seasons):
-    input_data['weights'] = input_data[weight_feature].values / input_data.loc[input_data['season'].isin(seasons), weight_feature].sum()
+    input_data["weights"] = (
+        input_data[weight_feature].values
+        / input_data.loc[input_data["season"].isin(seasons), weight_feature].sum()
+    )
     return input_data
 
 
-def prepare_data(data, train_seasons, val_seasons, test_seasons, features, response, weight_feature):
+def prepare_data(
+    data, train_seasons, val_seasons, test_seasons, features, response, weight_feature
+):
     full_data = data.copy()
 
     # Assign start indices for val and test data
     # start train index at first game of third season
-    train_start_index = full_data[full_data['season'] == (min(train_seasons) + 2)].index.min()
-    val_start_index = full_data[full_data['season'].isin(val_seasons)].index.min()
-    test_start_index = full_data[full_data['season'].isin(test_seasons)].index.min()
+    train_start_index = full_data[
+        full_data["season"] == (min(train_seasons) + 2)
+    ].index.min()
+    val_start_index = full_data[full_data["season"].isin(val_seasons)].index.min()
+    test_start_index = full_data[full_data["season"].isin(test_seasons)].index.min()
 
-    train_data = full_data[full_data['season'].isin(train_seasons)]
-    val_data = full_data[full_data['season'].isin(train_seasons + val_seasons)]
-    test_data = full_data[full_data['season'].isin(train_seasons + val_seasons + test_seasons)]
+    train_data = full_data[full_data["season"].isin(train_seasons)]
+    val_data = full_data[full_data["season"].isin(train_seasons + val_seasons)]
+    test_data = full_data[
+        full_data["season"].isin(train_seasons + val_seasons + test_seasons)
+    ]
 
     # scale features - use train data min and max
     feature_min = train_data[features].min()
     feature_max = train_data[features].max()
-    train_data[features] = (train_data[features] - feature_min) / (feature_max - feature_min)
-    val_data[features] = (val_data[features] - feature_min) / (feature_max - feature_min)
-    test_data[features] = (test_data[features] - feature_min) / (feature_max - feature_min)
+    train_data[features] = (train_data[features] - feature_min) / (
+        feature_max - feature_min
+    )
+    val_data[features] = (val_data[features] - feature_min) / (
+        feature_max - feature_min
+    )
+    test_data[features] = (test_data[features] - feature_min) / (
+        feature_max - feature_min
+    )
 
     # Create weights
     # make sure min in train_seasons is min(train_seasons) + 2
     min_season = min(train_seasons) + 2
-    train_data = create_weights(train_data, weight_feature, list(range(min_season, max(train_seasons) + 1)))
+    train_data = create_weights(
+        train_data, weight_feature, list(range(min_season, max(train_seasons) + 1))
+    )
     val_data = create_weights(val_data, weight_feature, val_seasons)
     test_data = create_weights(test_data, weight_feature, test_seasons)
 
     # Sequences
     train_seq = create_inout_sequences(train_data, features, response, start_index=1)
-    val_seq = create_inout_sequences(val_data, features, response, start_index=val_start_index)
-    test_seq = create_inout_sequences(test_data, features, response, start_index=test_start_index)
+    val_seq = create_inout_sequences(
+        val_data, features, response, start_index=val_start_index
+    )
+    test_seq = create_inout_sequences(
+        test_data, features, response, start_index=test_start_index
+    )
 
     # Create datasets
     train_dataset = SequenceDataset(train_seq)
@@ -92,12 +122,20 @@ def prepare_data(data, train_seasons, val_seasons, test_seasons, features, respo
 
     return train_dataset, val_dataset, test_dataset
 
+
 # Data Loaders Function
 def create_data_loaders(train_dataset, val_dataset, test_dataset, batch_size):
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn
+    )
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn
+    )
     return train_loader, val_loader, test_loader
+
 
 def collate_fn(batch):
     sequences, ext_data, weights, labels = zip(*batch)
@@ -108,6 +146,7 @@ def collate_fn(batch):
     labels = torch.stack(labels)
     return sequences_padded, ext_data, weights, labels, lengths
 
+
 # Custom Weighted MSE Loss
 def weighted_mse_loss(input, target, weight):
     # return (weight * (input - target) ** 2).sum()
@@ -116,7 +155,15 @@ def weighted_mse_loss(input, target, weight):
 
 # Causal CNN Model Class with Pack Padded Sequence
 class CausalCNNModel(nn.Module):
-    def __init__(self, feature_size, hidden_size, n_channels=[1, 5, 5, 1], kernel_size=[3, 3, 3], dilation=1, num_layers=2):
+    def __init__(
+        self,
+        feature_size,
+        hidden_size,
+        n_channels=[1, 5, 5, 1],
+        kernel_size=[3, 3, 3],
+        dilation=1,
+        num_layers=2,
+    ):
         super(CausalCNNModel, self).__init__()
 
         self.n_pad = [(k - 1) * dilation for k in kernel_size]
@@ -126,8 +173,11 @@ class CausalCNNModel(nn.Module):
         for i in range(len(n_channels) - 1):
             self.causal_cnn_blocks.append(
                 nn.Conv1d(
-                    in_channels=n_channels[i], out_channels=n_channels[i + 1],
-                    kernel_size=kernel_size[i], dilation=dilation, padding="same"
+                    in_channels=n_channels[i],
+                    out_channels=n_channels[i + 1],
+                    kernel_size=kernel_size[i],
+                    dilation=dilation,
+                    padding="same",
                 )
             )
         self.linear_layer = nn.Linear(4 * 5, hidden_size)
@@ -157,9 +207,10 @@ class CausalCNNModel(nn.Module):
 
         return prediction.squeeze()
 
+
 # Training Function
 def train_model(model, train_loader, val_loader, optimizer, epochs):
-    best_val_loss = float('inf')
+    best_val_loss = float("inf")
     best_model = None
     for epoch in range(epochs):
         model.train()
@@ -174,13 +225,16 @@ def train_model(model, train_loader, val_loader, optimizer, epochs):
             total_train_loss += loss.item()
 
         val_loss = evaluate_model(model, val_loader)
-        print(f'Epoch {epoch+1}/{epochs}, Training Loss: {total_train_loss}, Validation Loss: {val_loss}')
+        print(
+            f"Epoch {epoch+1}/{epochs}, Training Loss: {total_train_loss}, Validation Loss: {val_loss}"
+        )
 
         # Save the best model
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             best_model = model
     return best_val_loss, best_model
+
 
 # Evaluation Function
 def evaluate_model(model, loader):
@@ -193,16 +247,19 @@ def evaluate_model(model, loader):
             total_loss += loss.item()
     return total_loss
 
-def hyperparameter_tuning(data, train_seasons, val_seasons, test_seasons, features, response, param_grid=None):
+
+def hyperparameter_tuning(
+    data, train_seasons, val_seasons, test_seasons, features, response, param_grid=None
+):
     # Default hyperparameters
     default_params = {
-        'hidden_size': 32,
-        'num_layers': 2,
-        'lr': 5e-4,
-        'n_channels': [1, 5],
-        'kernel_size': [3],
-        'batch_size': 1,
-        'epochs': 500
+        "hidden_size": 32,
+        "num_layers": 2,
+        "lr": 5e-4,
+        "n_channels": [1, 5],
+        "kernel_size": [3],
+        "batch_size": 1,
+        "epochs": 500,
     }
 
     if param_grid is None or not param_grid:
@@ -211,18 +268,39 @@ def hyperparameter_tuning(data, train_seasons, val_seasons, test_seasons, featur
 
     best_model = None
     best_params = {}
-    best_val_loss = float('inf')
-    
+    best_val_loss = float("inf")
+
     for params in itertools.product(*param_grid.values()):
         param_dict = dict(zip(param_grid.keys(), params))
         print("Testing with parameters:", param_dict)
-        model = CausalCNNModel(feature_size=len(features), hidden_size=param_dict['hidden_size'], num_layers=param_dict['num_layers'], n_channels=param_dict["n_channels"], kernel_size=param_dict["kernel_size"])
-        optimizer = optim.Adam(model.parameters(), lr=param_dict['lr'])
+        model = CausalCNNModel(
+            feature_size=len(features),
+            hidden_size=param_dict["hidden_size"],
+            num_layers=param_dict["num_layers"],
+            n_channels=param_dict["n_channels"],
+            kernel_size=param_dict["kernel_size"],
+        )
+        optimizer = optim.Adam(model.parameters(), lr=param_dict["lr"])
 
-        train_dataset, val_dataset, test_dataset = prepare_data(data, train_seasons, val_seasons, test_seasons, features, response, weight_feature='fga')
-        train_loader, val_loader, test_dataset = create_data_loaders(train_dataset, val_dataset, test_dataset, batch_size=param_dict['batch_size'])
+        train_dataset, val_dataset, test_dataset = prepare_data(
+            data,
+            train_seasons,
+            val_seasons,
+            test_seasons,
+            features,
+            response,
+            weight_feature="fga",
+        )
+        train_loader, val_loader, test_dataset = create_data_loaders(
+            train_dataset,
+            val_dataset,
+            test_dataset,
+            batch_size=param_dict["batch_size"],
+        )
 
-        val_loss, model = train_model(model, train_loader, val_loader, optimizer, param_dict['epochs'])
+        val_loss, model = train_model(
+            model, train_loader, val_loader, optimizer, param_dict["epochs"]
+        )
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
@@ -232,13 +310,32 @@ def hyperparameter_tuning(data, train_seasons, val_seasons, test_seasons, featur
     print(f"Best Parameters: {best_params}, Best Validation Loss: {best_val_loss}")
     return best_model, best_params
 
-# Function to Run the Model with Best Parameters and Save Test Predictions
-def run_and_save_predictions(data, train_seasons, val_seasons, test_seasons, features, response, best_params):
-    train_dataset, val_dataset, test_dataset = prepare_data(data, train_seasons, val_seasons, test_seasons, features, response, weight_feature='fga')
-    train_loader, val_loader, test_loader = create_data_loaders(train_dataset, val_dataset, test_dataset, batch_size=best_params['batch_size'])
 
-    model = CausalCNNModel(feature_size=len(features), hidden_size=best_params['hidden_size'], num_layers=best_params['num_layers'], n_channels=best_params["n_channels"], kernel_size=best_params["kernel_size"])
-    model.load_state_dict(torch.load('best_model_cnn.pth'))
+# Function to Run the Model with Best Parameters and Save Test Predictions
+def run_and_save_predictions(
+    data, train_seasons, val_seasons, test_seasons, features, response, best_params
+):
+    train_dataset, val_dataset, test_dataset = prepare_data(
+        data,
+        train_seasons,
+        val_seasons,
+        test_seasons,
+        features,
+        response,
+        weight_feature="fga",
+    )
+    train_loader, val_loader, test_loader = create_data_loaders(
+        train_dataset, val_dataset, test_dataset, batch_size=best_params["batch_size"]
+    )
+
+    model = CausalCNNModel(
+        feature_size=len(features),
+        hidden_size=best_params["hidden_size"],
+        num_layers=best_params["num_layers"],
+        n_channels=best_params["n_channels"],
+        kernel_size=best_params["kernel_size"],
+    )
+    model.load_state_dict(torch.load("best_model_cnn.pth"))
 
     # Save Test Predictions
     model.eval()
@@ -248,17 +345,37 @@ def run_and_save_predictions(data, train_seasons, val_seasons, test_seasons, fea
             y_pred = model(seq, ext_data, lengths)
             test_predictions += [y_pred.tolist()]
 
-    pd.concat([
-        data[data.season.isin(test_seasons)][["league_avg_fg3a_fga", "fga"]].reset_index(),
-        pd.DataFrame(test_predictions, columns=['Predictions']).reset_index()
-    ], axis=1).to_csv('cnn_test_predictions.csv', index=False)
+    pd.concat(
+        [
+            data[data.season.isin(test_seasons)][
+                ["league_avg_fg3a_fga", "fga"]
+            ].reset_index(),
+            pd.DataFrame(test_predictions, columns=["Predictions"]).reset_index(),
+        ],
+        axis=1,
+    ).to_csv("cnn_test_predictions.csv", index=False)
     print("Test predictions saved to 'test_predictions.csv'.")
 
 
 # Run the Model with Best Parameters and Save Test Predictions
 data = league_data_loader(seasons=list(range(2010, 2020)))
-features = ['season_type', 'date_num']
-best_model, best_params = hyperparameter_tuning(data, list(range(2010, 2015)), list(range(2015, 2016)), list(range(2016, 2020)), features, 'league_avg_fg3a_fga', param_grid=None)
-torch.save(best_model.state_dict(), 'best_model_cnn.pth')
-run_and_save_predictions(data, list(range(2010, 2015)), list(range(2015, 2016)), list(range(2016, 2020)), features, 'league_avg_fg3a_fga', best_params)
-
+features = ["season_type", "date_num"]
+best_model, best_params = hyperparameter_tuning(
+    data,
+    list(range(2010, 2015)),
+    list(range(2015, 2016)),
+    list(range(2016, 2020)),
+    features,
+    "league_avg_fg3a_fga",
+    param_grid=None,
+)
+torch.save(best_model.state_dict(), "best_model_cnn.pth")
+run_and_save_predictions(
+    data,
+    list(range(2010, 2015)),
+    list(range(2015, 2016)),
+    list(range(2016, 2020)),
+    features,
+    "league_avg_fg3a_fga",
+    best_params,
+)
--- modeling/causal_cnn/cnn.py
+++ modeling/causal_cnn/cnn.py
@@ -12,9 +12,10 @@ from sklearn.metrics import mean_squared_error, r2_score
 import sys
 
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from data_handling.data_loading import league_data_loader
 
@@ -24,15 +25,15 @@ data = league_data_loader(seasons=list(range(2010, 2020)))
 
 # Split into training sequence, test sequence
 
+
 def get_x_y(seq, lag):
-	x = np.array(
-		[seq[start:(start + lag)] for start in range(len(seq) - lag)]
-    )
-	y = np.array([seq[pos] for pos in range(lag, len(seq))])
-	return x, y
+    x = np.array([seq[start : (start + lag)] for start in range(len(seq) - lag)])
+    y = np.array([seq[pos] for pos in range(lag, len(seq))])
+    return x, y
+
 
 train_seq = np.array(
-	data[data.season.isin(list(range(2010,2015)))].league_avg_fg3a_fga.tolist()
+    data[data.season.isin(list(range(2010, 2015)))].league_avg_fg3a_fga.tolist()
 )
 # Same as AR model
 lag = 4
@@ -40,20 +41,22 @@ lag = 4
 x, y = get_x_y(train_seq, lag=lag)
 x = x.reshape((x.shape[0], x.shape[1], 1))
 
-# Basic model
-model = Sequential()
-model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(lag, 1)))
-model.add(Flatten())
-model.add(Dense(50, activation='relu'))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
-model.fit(x, y, epochs=1000, verbose=0)
+# Basic CNN model
+causal_cnn = Sequential()
+causal_cnn.add(
+    Conv1D(filters=128, kernel_size=3, activation="relu", input_shape=(lag, 1))
+)
+causal_cnn.add(Flatten())
+causal_cnn.add(Dense(64, activation="relu"))
+causal_cnn.add(Dense(1))
+causal_cnn.compile(optimizer="adam", loss="mse")
+causal_cnn.fit(x, y, epochs=500, verbose=0)
 
 test_seq = np.array(
-	data[data.season.isin(list(range(2016,2020)))].league_avg_fg3a_fga.tolist()
+    data[data.season.isin(list(range(2016, 2020)))].league_avg_fg3a_fga.tolist()
 )
 test_x, test_y = get_x_y(test_seq, lag)
 test_x = test_x.reshape((test_x.shape[0], test_x.shape[1], 1))
-yhats = model.predict(test_x)
+yhats = causal_cnn.predict(test_x)
 print(mean_squared_error(yhats, test_y))
 print(r2_score(y_true=test_y, y_pred=yhats))
